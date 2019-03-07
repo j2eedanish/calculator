a34830449acfbe94f0f2d7fc9eb1b1808769f681
j2eedanish <j2eedanish@gmail.com>
2019-03-07 11:15:56 +0530

    Final

--- src/com/danish/ideationzCalculator/Calculator.java
+++ src/com/danish/ideationzCalculator/Calculator.java
@@ -4,21 +4,37 @@ import java.util.Scanner;
 
 public class Calculator extends CollectData {
 	public static void main(String[] args) {
-
+		double output;
     	CollectData data=new CollectData();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number:");
-
-        data.setNum1(scanner.nextDouble()); 
+        try {
+        data.setNum1(scanner.nextDouble());
+        	
+        }
+        catch(Exception e) {
+        	System.out.println("Please enter number only");
+        	System.exit(0);
+        }
         System.out.print("Enter second number:");
+        try {
         data.setNum2(scanner.nextDouble());
+        }
+        catch(Exception e) {
+        	System.out.println("Please enter number only");
+        	System.exit(0);
+        }
 
         System.out.print("Enter an operator (+, -, *, /): ");
         data.setOperator(scanner.next().charAt(0));
 
         scanner.close();
-        double output;
-
+        if(data.getOperator()=='/' && data.getNum2()==0) {
+        	System.out.println("can not divid by 0");
+        	
+        }
+        
+        else {
         switch(data.getOperator())
         {
             case '+':
@@ -35,12 +51,12 @@ public class Calculator extends CollectData {
 
             case '/':
             	output = data.getNum1() / data.getNum1();
-                break;
+            	break;
             default:
                 System.out.printf("You have entered wrong operator");
                 return;
         }
-
-        System.out.println(data.getNum1()+" "+data.getOperator()+" "+data.getNum2()+": "+output);
+          System.out.println(data.getNum1()+" "+data.getOperator()+" "+data.getNum2()+": "+output);
     }
+	}
 }
